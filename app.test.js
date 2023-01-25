771891b24f7405abaefb741b3196df7f9f02fc0c
Ryan <ryanlearyzafra@gmail.com>
2023-01-25 23:10:43 +0100

    Finished tests

--- app.test.js
+++ app.test.js
@@ -1,11 +1,14 @@
-import { lista } from './app.js';
+import { lista } from './asd';
 
 describe('Given index function', () => {
   describe('When the inset the number they are looking for', () => {
     test('When insert 3 it should show 3', () => {
       const a = 3;
-      const expected = 3;
-      const r = lista(a);
+      const b = [1, 2, 3, 4];
+      const c = 0;
+      const index = 2;
+      const expected = 2;
+      const r = lista(a, b, c, index);
       expect(r).toBe(expected);
     });
   });
