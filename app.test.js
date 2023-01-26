c61c2e8f87d6f20649ff05266a6229d1e1adc736
Ryan <ryanlearyzafra@gmail.com>
2023-01-26 18:04:43 +0100

    Added ignire to test

--- app.test.js
+++ app.test.js
@@ -1,7 +1,10 @@
 import { findIndex } from './asd';
 
+// eslint-disable-next-line no-undef
 describe('Given findIndex function', () => {
+  // eslint-disable-next-line no-undef
   describe('When the inset the number they are looking for', () => {
+    // eslint-disable-next-line no-undef
     test('When insert 3 it should show 3', () => {
       const a = 3;
       const b = [1, 2, 3, 4];
@@ -9,6 +12,7 @@ describe('Given findIndex function', () => {
       const index = 2;
       const expected = 2;
       const r = findIndex(a, b, c, index);
+      // eslint-disable-next-line no-undef
       expect(r).toBe(expected);
     });
   });
