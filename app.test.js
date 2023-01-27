dd8a12301fae7c5aa1d3071fd7fea1bc5ee0127e
Ryan <ryanlearyzafra@gmail.com>
2023-01-27 07:58:05 +0100

    Changed values 2

--- app.test.js
+++ app.test.js
@@ -1,13 +1,13 @@
-import { findIndex } from './asd';
+import { lengths } from './asd';
 
 // eslint-disable-next-line no-undef
 describe('Given findIndex function', () => {
   // eslint-disable-next-line no-undef
   test('When insert 3 it should show 3', () => {
-    const a = 2;
-    const b = 'hola';
-    const expected = 'h';
-    const r = findIndex(a, b);
+    const arr = 3;
+    const x = [1, 2, 3, 4];
+    const expected = 4;
+    const r = lengths(x, arr);
     // eslint-disable-next-line no-undef
     expect(r).toBe(expected);
   });
--- asd.js
+++ asd.js
@@ -3,16 +3,3 @@ export const lengths = (x) => {
   while (x[arr] !== undefined) arr++;
   return arr;
 };
-
-export const findIndex = (a, x) => {
-  let count = 0;
-
-  for (a of x) {
-    if (a !== lengths(x)) {
-      count += 1;
-    }
-
-    count = a[count];
-    return a;
-  }
-};
